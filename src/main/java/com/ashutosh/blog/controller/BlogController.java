9f5e144d3441aa1b24726850de8296ef95d549a7
Ashutosh Kalra <ashutoshkalra7@gmail.com>
2024-03-15 00:58:42 +0530

    delete method added in controller and deletepage is also added

--- src/main/java/com/ashutosh/blog/controller/BlogController.java
+++ src/main/java/com/ashutosh/blog/controller/BlogController.java
@@ -68,7 +68,7 @@ public class BlogController {
                 String name = tag.getName();
                 Tag newTag = tagService.findByName(name);
                 post.addtags(newTag);
-                System.out.println("already present "+name);
+                System.ougit t.println("already present "+name);
             }
         }
         postService.save(post);
@@ -82,4 +82,49 @@ public class BlogController {
         theModel.addAttribute("post" , post);
         return "Blog-Post";
     }
+       
+    @PostMapping("/updateblog")
+    public String updateBlog(@ModelAttribute("post") Post post, Model theModel){
+        User user = post.getAuthor();
+        List<Tag> tags = post.getTags();
+        List<Tag> alltags = tagService.findAll();
+        Set<String> tagNameinDb = new HashSet<>();
+        for(Tag tag :alltags){
+            tagNameinDb.add(tag.getName());
+        }
+        post.setTags("");
+        for(Tag tag : tags){
+            if(!tagNameinDb.contains(tag.getName())){
+                Tag newTag = new Tag(tag.getName());
+                post.addtags(newTag);
+            }
+            else{
+                String name = tag.getName();
+                Tag newTag = tagService.findByName(name);
+                post.addtags(newTag);
+            }
+        }
+        postService.update(post.getId(), post.getTitle(), post.getContent());
+        List<Post> posts = postService.findAll();
+        theModel.addAttribute("blogs", posts);
+        return "Home-Page";
+    }
     
+    
+    
+    @GetMapping("/updateblog/{postId}")
+    public String updatePost(@PathVariable("postId") int id, Model theModel){
+        Post post = postService.findById(id);
+        theModel.addAttribute("post" , post);
+        return "update-post";
+    }
+    
+    @GetMapping("/deleteblog/{postId}")
+    public String deletePost(@PathVariable("postId") int id, Model theModel){
+        Post post = postService.findById(id);
+        postService.delete(post);
+        List<Post> posts = postService.findAll();
+        theModel.addAttribute("blogs", posts);
+        return "Home-Page";
+    }
+}
